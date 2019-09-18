a0452238e838f4e02322b2182bd8bfa67af31488
tehpsalmist <bstewardcodes@gmail.com>
2019-09-18 13:05:07 -0400

    include stats

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,12 @@ import beachbaby from '../assets/beachbaby-square.jpg'
 export const Header = props => {
   return <header className='flex flex-col items-center text-center py-4'>
     <h1 className='text-3xl mb-4'>
-      <span className='domestic-manners'>Olivia Rae</span> is almost here!
+      <span className='domestic-manners'>Olivia Rae</span> is here!
     </h1>
     <h4>
-      How big will she be? When will she arrive? Who is her favorite power ranger? So many questions!
+      The Verdict:<br />8lbs. 2oz., 18.75in., with a birthday of 9/17 at 7:18PM!
     </h4>
     <img className='rounded-full w-5/6 max-w-90 shadow-lg mt-4' src={beachbaby} alt='Baby Olivia spots her first whale' />
+    <a className='mt-6 p-4 text-xl bg-green-500 shadow-lg rounded' href='https://www.facebook.com/aljones3157/posts/10157798357397792' target='_blank'>See Pictures!</a>
   </header>
 }
